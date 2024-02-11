61653936076bdbd5688276368767b08ae6c7fbfe
n8allan <3095770+n8allan@users.noreply.github.com>
2024-02-10 17:42:56 -0700

    Checked to isValid

--- src/cryptohash.ts
+++ src/cryptohash.ts
@@ -1,6 +1,6 @@
 export interface CryptoHash {
 	/** Ensures that the given salt passes all of the tests for a Code */
-	codeValid(code: string): boolean;
+	isValid(code: string): boolean;
 
 	/** Generates a unique Code with sufficient entropy based on the given options.
  	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
