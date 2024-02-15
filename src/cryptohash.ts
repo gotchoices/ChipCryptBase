78ab07be2f050c9ffd86a7be2c0dbe2de87a67ee
n8allan <3095770+n8allan@users.noreply.github.com>
2024-02-14 23:12:47 -0700

    Added optional now arguments to cryptohash.

--- src/cryptohash.ts
+++ src/cryptohash.ts
@@ -1,12 +1,15 @@
 /** Interface for generating and validating secure hashes (Nonces) using highly random salt values (Codes) */
 export interface CryptoHash {
-	/** Ensures that the given Code (salt) passes all of the tests for high randomness. */
-	isValid(code: string): boolean;
+	/** Ensures that the given Code (salt) passes all of the tests for high randomness.
+	 * @now The current time in milliseconds since the epoch.  This is used to validate the expiration of the Code.
+	*/
+	isValid(code: string, now?: number): boolean;
 
 	/** Generates a unique Code with sufficient entropy based on the given options.
- 	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
- 	 */
-	generate(): Promise<string>;
+	 * @now The current time in milliseconds since the epoch.  This is used to calculate the expiration of the Code.
+	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
+	 */
+	generate(now?: number): Promise<string>;
 
 	/** Generate anonymized payload hash using a Code as a salt */
 	makeNonce(payload: string, code: string): Promise<string>;
