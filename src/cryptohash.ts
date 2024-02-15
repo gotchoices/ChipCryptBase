d5c38491097ecf8cb717be257a5e6864e86c1689
n8allan <3095770+n8allan@users.noreply.github.com>
2024-02-15 11:43:16 -0700

    Added ...Bin variants to cryptohash

--- src/cryptohash.ts
+++ src/cryptohash.ts
@@ -1,19 +1,57 @@
 /** Interface for generating and validating secure hashes (Nonces) using highly random salt values (Codes) */
 export interface CryptoHash {
-	/** Ensures that the given Code (salt) passes all of the tests for high randomness.
-	 * @now The current time in milliseconds since the epoch.  This is used to validate the expiration of the Code.
-	*/
+	/**
+	 * Ensures that the given Code (salt) passes all of the tests for high randomness and expiration.
+	 * @param now The current time in milliseconds since the epoch. This is used to validate the expiration of the Code.
+	 */
 	isValid(code: string, now?: number): boolean;
 
-	/** @returns the expiration date/time (ms since Unix epoch) of the given code.  Note: this may not be to ms granularity */
+	/**
+	 * Ensures that the given Code (salt) passes all of the tests for high randomness.
+	 * @param codeBin The binary representation of the code to validate.
+	 * @param now The current time in milliseconds since the epoch. This is used to validate the expiration of the Code.
+	 */
+	isValidBin(codeBin: Uint8Array, now?: number): boolean;
+
+	/**
+	 * @returns The expiration date/time in milliseconds since the Unix epoch. Note: this may not be to millisecond granularity.
+	 */
 	getExpiration(code: string): number;
 
-	/** Generates a unique Code with sufficient entropy based on the given options.
-	 * @now The current time in milliseconds since the epoch.  This is used to calculate the expiration of the Code.  Note: may not be stored to ms granularity.
+	/**
+	 * @param codeBin The binary representation of the code to get the expiration for.
+	 * @returns The expiration date/time in milliseconds since the Unix epoch. Note: this may not be to millisecond granularity.
+	 */
+	getExpirationBin(codeBin: Uint8Array): number;
+
+	/**
+	 * Generates a unique Code with sufficient entropy based on the given options.
+	 * @param now The current time in milliseconds since the epoch. This is used to calculate the expiration of the Code. Note: may not be stored to millisecond granularity.
 	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
 	 */
 	generate(now?: number): Promise<string>;
 
-	/** Generate anonymized payload hash using a Code as a salt */
+	/**
+	 * Generates a unique Code with sufficient entropy based on the given options.
+	 * @param now The current time in milliseconds since the epoch. This is used to calculate the expiration of the Code. Note: may not be stored to millisecond granularity.
+	 * @returns A promise that resolves to the binary representation of the generated code.
+	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
+	 */
+	generateBin(now?: number): Promise<Uint8Array>;
+
+	/**
+	 * Generates an anonymized payload hash using a Code as a salt.
+	 * @param payload The payload to hash.
+	 * @param code The code to use as a salt.
+	 * @returns A promise that resolves to the generated nonce.
+	 */
 	makeNonce(payload: string, code: string): Promise<string>;
+
+	/**
+	 * Generates an anonymized payload hash using a Code as a salt.
+	 * @param payloadBin The binary representation of the payload to hash.
+	 * @param codeBin The binary representation of the code to use as a salt.
+	 * @returns A promise that resolves to the binary representation of the generated nonce.
+	 */
+	makeNonceBin(payloadBin: Uint8Array, codeBin: Uint8Array): Promise<Uint8Array>;
 }
