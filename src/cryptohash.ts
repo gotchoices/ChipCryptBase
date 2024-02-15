e9e3b082af79b60fd01bc89ef89a100da0077579
n8allan <3095770+n8allan@users.noreply.github.com>
2024-02-15 10:37:47 -0700

    Add getExpiration method to CryptoHash interface

--- src/cryptohash.ts
+++ src/cryptohash.ts
@@ -5,8 +5,11 @@ export interface CryptoHash {
 	*/
 	isValid(code: string, now?: number): boolean;
 
+	/** @returns the expiration date/time (ms since Unix epoch) of the given code.  Note: this may not be to ms granularity */
+	getExpiration(code: string): number;
+
 	/** Generates a unique Code with sufficient entropy based on the given options.
-	 * @now The current time in milliseconds since the epoch.  This is used to calculate the expiration of the Code.
+	 * @now The current time in milliseconds since the epoch.  This is used to calculate the expiration of the Code.  Note: may not be stored to ms granularity.
 	 * @throws An error if a Code with sufficient entropy cannot be generated within the maximum number of tries specified in the options.
 	 */
 	generate(now?: number): Promise<string>;
